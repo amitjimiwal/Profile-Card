0d350ff1dffb5882ba744dd48e115cbc93f0425a
Amit <90555965+amitjimiwal@users.noreply.github.com>
2022-12-23 15:44:16 +0530

    Updated About component

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,10 @@ export default function Main(){
       return (
             <div className='main'>
                   <h3>About</h3>
-                  <p>I am a frontend Developer with knowledge of Html,css,JS and ReactJS. I am an enthusiast developer.
+                  <p>I am a frontend Developer skilled in HTML ,CSS ,Javascript and ReactJS.<br></br>
                    "Learn what you love and built what is needed".</p>
                   <h3>Interests</h3>
-                  <p>Machine Learning, Data structures,Problem Solving</p>
+                  <p>Machine Learning, Data structures,Problem Solving.</p>
             </div>
       )
 }
